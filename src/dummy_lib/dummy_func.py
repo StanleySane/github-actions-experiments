a634687a0bed5bc4ad0b2eaab28e8fadddbed577
Stanley Sane <stanleysane@gmail.com>
2021-08-25 15:04:57 +0300

    Fix for linter success

--- src/dummy_lib/dummy_func.py
+++ src/dummy_lib/dummy_func.py
@@ -9,6 +9,10 @@ logging.getLogger().addHandler(logging.NullHandler())
 
 
 def dummy_func(msg: str) -> str:
+    """
+    :param msg: incoming message
+    :return: echo message
+    """
     logging.info(f"Inside dummy_func with {msg!r}")
 
     return f"Echo {msg!r}"
